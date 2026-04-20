319db1cf7ad66cf845ef9a21c4e369e9c7af1696
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-04-20 18:25:30 +0530

    updatev databse

--- config/settings.py
+++ config/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',   # tamaru DB name
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
