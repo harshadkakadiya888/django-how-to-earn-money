058c22e58154a4a05871e1641852464265b61840
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-04-21 15:03:20 +0530

    setting change

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
