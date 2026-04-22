06ca96852bca848c296f96cfae4bfc534cbcb982
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-04-23 00:40:20 +0530

    admin

--- blog/apps.py
+++ blog/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
 
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='admin@example.com',
-                password='12345678'
-            )
-            print("Superuser created")
