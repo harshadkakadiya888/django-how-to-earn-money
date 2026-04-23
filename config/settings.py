555217c516e5e4c52f124464090bebdce7827862
harshadkakadiya888 <harshadkakadiya888@gmail.com>
2026-04-23 17:16:32 +0530

    add cloudinary packages

--- config/settings.py
+++ config/settings.py
@@ -156,8 +156,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
